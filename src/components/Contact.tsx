4dcc83ce0d11bc9aca489ef45088ad864b78bc4c
MFaisal125 <mfaisal0125470@gmail.com>
2024-09-19 05:57:01 +0500

    Portfolio2

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -20,7 +20,7 @@ const Contact = () => {
       <TitleText title="Contact Us" />
       <div className="grid grid-cols-1 md:grid-cols-2">
         <motion.div
-          variants={staggerContainer}
+          variants={staggerContainer(0.1, 0.2)}
           initial="hidden"
           whileInView="show"
           viewport={{ once: false, amount: 0.25 }}
@@ -111,7 +111,7 @@ const Contact = () => {
         </motion.div>
 
         <motion.div
-          variants={staggerContainer}
+          variants={staggerContainer(0.2, 0.5)}
           initial="hidden"
           whileInView="show"
           viewport={{ once: false, amount: 0.25 }}
